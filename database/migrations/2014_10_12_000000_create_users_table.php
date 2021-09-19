af106572b957652f189694fc0ff8fd26a1257cb4
Vũ Tuân <75652084+vuhuyentuan@users.noreply.github.com>
2021-09-19 17:03:13 +0700

    update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->integer('role')->default(0);
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
